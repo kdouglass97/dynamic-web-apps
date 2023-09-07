faa5061f73e7d551e7f31ff64a547e5427b31d10
kdouglass97 <66433917+kdouglass97@users.noreply.github.com>
2023-09-07 09:59:12 -0400

    exc 1

---
+++ a.js
@@ -0,0 +1,34 @@
+//variables
+
+//vey rarely will you use this
+var nameOfVariable = ''
+
+//once set will never change
+//only matters in the scope of the function
+const constantVariable = 'constant'
+
+//changeable variable; types/ values
+let changeableVariable = 'something that\'s going to change'
+
+console.log(changeableVariable)
+changeableVariable = 'something'
+console.log(changeableVariable)
+
+
+//arrays
+const coolArray =[
+    'string',
+    0,
+    {},
+    0.4
+]
+
+//will make the thing inside the array change but the actual array and type will never change
+console.log(coolArray(4))
+coolArray[0] = 'another string'
+//push, filter, index
+
+//Numbers
+123
+//floating points
+.23
