4c7f43aee64feda7817746911b67092b5f9b7120
kdouglass97 <66433917+kdouglass97@users.noreply.github.com>
2023-09-12 09:30:10 -0400

    lecture3

---
+++ objectsandFunctions.js
@@ -0,0 +1,36 @@
+##variables
+    let
+    const
+
+   
+##functions
+// both are called anonymous functions
+
+function coolFunction(arg){
+    if(typeof arg=== 'string') return 'string';
+    return 'not string';
+}
+coolFunction('yardddd')
+
+const coolArrowFunction() => 'hello';
+
+
+##objects
+
+{
+    "key": "value"
+    key: "value"
+}
+
+const annoyingObject = {
+    top: [
+        {middle: {
+            bottom: { 
+                thing: 'frogs'
+            }
+        }}
+    ]
+}
+
+annoyingObject.top[0].middle.bottom.thing // 'frogs'
+
